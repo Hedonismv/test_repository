73758f6301029a823df7bb4c7b449e93e62991d0
Vitaly Klyhun <klygun90@gmal.com>
2020-12-23 00:31:58 +0300

    5s exc completed

---
+++ hw03-5.py
@@ -0,0 +1,12 @@
+
+def rectangle():
+    a = int(input("1: "))
+    b = int(input("2: "))
+
+    area = (a * b) / 2
+    gip = (a ** 2) + (b ** 2) / 
+
+    print(f"Площадь:{area}\nГипотенуза:{gip}")
+
+if __name__ == "__main__":
+    rectangle()
