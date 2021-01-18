5cebedd211ca5ec8b5787ce6f6f088e063f43ccd
Vitaly Klyhun <klygun90@gmal.com>
2021-01-18 16:16:37 +0300

    exc 5 completed

---
+++ hw07_05.py
@@ -0,0 +1,9 @@
+#Дан список строк. Отформатировать все строки в
+#формате ‘{i} - {string}’, где i это порядковы номер
+#строки в списке. Использовать генератор списков.
+words = 'string','word','forar','grades'
+lists = [a for a in words]
+i = 1
+for string in lists:
+    print(f"{i} - {string}")
+    i += 1
