a8c767464da2245a1bb480567374ef883eb4a445
Vitaly Klyhun <klygun90@gmal.com>
2021-02-21 13:48:27 +0300

    add the psycopg2 version

---
+++ database/db.py
@@ -0,0 +1,97 @@
+import psycopg2
+
+try:
+    conn = psycopg2.connect(dbname='gui', user='postgres', password='123', host='localhost')  # db authorization
+    cursor = conn.cursor()
+    print('Подключение прошло успешно')
+except ConnectionError:
+    print('Не удалось подключиться к базе данных')
+
+
+def add_the_car(*car):
+    try:
+        cursor.execute("INSERT INTO "
+                       "car (brand_id, car_model, car_release_year)"
+                       " VALUES(?, ?, ?);", car)
+        conn.commit()
+        print('Машина успешно добавлена в базу данных')
+    except Exception as e:
+        print(F'Error {e} occurred')
+
+
+def add_the_brand(name):
+    try:
+        cursor.execute("INSERT INTO "
+                       "brand (brand_name)"
+                       " VALUES(?);", name)
+        conn.commit()
+        print('Бренд успешно добавлен в базу данных')
+    except Exception as e:
+        print(F'Error {e} occurred')
+
+
+def read_the_car():
+    cursor.execute("SELECT brand_name, car_model, car_release_year FROM brand"
+                   " INNER JOIN gui.public.car ON brand.brand_id = car.brand_id ")
+    car_list = cursor.fetchall()
+    for brand_name, car_model, car_release_year in car_list:
+        print(  # f'\nID Продукта - {p_id}\n'
+            f'***********************************\n'
+            f'Название Бренда - {brand_name}\n'
+            f'Модель автомобиля - {car_model}\n'
+            f'Год выпуска - {car_release_year}\n'
+            f'***********************************\n')
+    conn.commit()
+
+
+def read_the_brand():
+    cursor.execute("SELECT * FROM brand")
+    brand_list = cursor.fetchall()
+    for brand_id, brand_name in brand_list:
+        print(  # f'\nID Продукта - {p_id}\n'
+            f'***********************************\n'
+            f'ID Бренда - {brand_id}'
+            f'Название Бренда - {brand_name}\n'
+            f'***********************************\n')
+    conn.commit()
+
+
+def update_the_car(c_id, car_m_name):
+    cur.execute(f"""UPDATE car SET car_model = {car_m_name} WHERE car_id = {c_id} """)
+    conn.commit()
+    print('Модель авто заменена')
+
+
+def update_the_brand(b_id, b_name):
+    cur.execute(f"""UPDATE brand SET brand_name = {b_name} WHERE brand_id = {b_id} """)
+    conn.commit()
+    print('Название бренда заменено')
+
+
+def delete_the_car(c_id):
+    cur.execute(f"""DELETE FROM car WHERE gui.public.car.car_id = {c_id}""")
+    conn.commit()
+    print('Авто удалено')
+
+
+def delete_the_brand(b_id):
+    cur.execute(f"""DELETE FROM brand WHERE brand_id = {b_id}""")
+    conn.commit()
+    print('Бренд удален')
+# cursor.execute("""CREATE TABLE IF NOT EXISTS brand(
+#     brand_id SERIAL PRIMARY KEY ,
+#     brand_name TEXT NOT NULL UNIQUE);
+# """)
+# conn.commit()
+#
+# cursor.execute("""CREATE TABLE IF NOT EXISTS car(
+#     car_id SERIAL PRIMARY KEY ,
+#     brand_id INTEGER,
+#     car_model TEXT NOT NULL ,
+#     car_release_year INTEGER NOT NULL ,
+#     CONSTRAINT fk_brand
+#         FOREIGN KEY (brand_id)
+#             REFERENCES brand(brand_id)
+#                 ON DELETE CASCADE );
+# """)
+# conn.commit()
---
+++ dump.sql
@@ -0,0 +1,177 @@
+--
+-- PostgreSQL database dump
+--
+
+-- Dumped from database version 12.6 (Ubuntu 12.6-1.pgdg20.04+1)
+-- Dumped by pg_dump version 13.2 (Ubuntu 13.2-1.pgdg20.04+1)
+
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET idle_in_transaction_session_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SELECT pg_catalog.set_config('search_path', '', false);
+SET check_function_bodies = false;
+SET xmloption = content;
+SET client_min_messages = warning;
+SET row_security = off;
+
+SET default_tablespace = '';
+
+SET default_table_access_method = heap;
+
+--
+-- Name: brand; Type: TABLE; Schema: public; Owner: postgres
+--
+
+CREATE TABLE public.brand (
+    brand_id integer NOT NULL,
+    brand_name text NOT NULL
+);
+
+
+ALTER TABLE public.brand OWNER TO postgres;
+
+--
+-- Name: brand_brand_id_seq; Type: SEQUENCE; Schema: public; Owner: postgres
+--
+
+CREATE SEQUENCE public.brand_brand_id_seq
+    AS integer
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+ALTER TABLE public.brand_brand_id_seq OWNER TO postgres;
+
+--
+-- Name: brand_brand_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: postgres
+--
+
+ALTER SEQUENCE public.brand_brand_id_seq OWNED BY public.brand.brand_id;
+
+
+--
+-- Name: car; Type: TABLE; Schema: public; Owner: postgres
+--
+
+CREATE TABLE public.car (
+    car_id integer NOT NULL,
+    brand_id integer,
+    car_model text NOT NULL,
+    car_release_year integer NOT NULL
+);
+
+
+ALTER TABLE public.car OWNER TO postgres;
+
+--
+-- Name: car_car_id_seq; Type: SEQUENCE; Schema: public; Owner: postgres
+--
+
+CREATE SEQUENCE public.car_car_id_seq
+    AS integer
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+ALTER TABLE public.car_car_id_seq OWNER TO postgres;
+
+--
+-- Name: car_car_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: postgres
+--
+
+ALTER SEQUENCE public.car_car_id_seq OWNED BY public.car.car_id;
+
+
+--
+-- Name: brand brand_id; Type: DEFAULT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.brand ALTER COLUMN brand_id SET DEFAULT nextval('public.brand_brand_id_seq'::regclass);
+
+
+--
+-- Name: car car_id; Type: DEFAULT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.car ALTER COLUMN car_id SET DEFAULT nextval('public.car_car_id_seq'::regclass);
+
+
+--
+-- Data for Name: brand; Type: TABLE DATA; Schema: public; Owner: postgres
+--
+
+COPY public.brand (brand_id, brand_name) FROM stdin;
+3	Mercedes-Benz
+4	Audi
+\.
+
+
+--
+-- Data for Name: car; Type: TABLE DATA; Schema: public; Owner: postgres
+--
+
+COPY public.car (car_id, brand_id, car_model, car_release_year) FROM stdin;
+2	3	e220	2016
+3	4	A4	2010
+4	4	A8	2020
+\.
+
+
+--
+-- Name: brand_brand_id_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
+--
+
+SELECT pg_catalog.setval('public.brand_brand_id_seq', 4, true);
+
+
+--
+-- Name: car_car_id_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
+--
+
+SELECT pg_catalog.setval('public.car_car_id_seq', 4, true);
+
+
+--
+-- Name: brand brand_brand_name_key; Type: CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.brand
+    ADD CONSTRAINT brand_brand_name_key UNIQUE (brand_name);
+
+
+--
+-- Name: brand brand_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.brand
+    ADD CONSTRAINT brand_pkey PRIMARY KEY (brand_id);
+
+
+--
+-- Name: car car_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.car
+    ADD CONSTRAINT car_pkey PRIMARY KEY (car_id);
+
+
+--
+-- Name: car fk_brand; Type: FK CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.car
+    ADD CONSTRAINT fk_brand FOREIGN KEY (brand_id) REFERENCES public.brand(brand_id) ON DELETE CASCADE;
+
+
+--
+-- PostgreSQL database dump complete
+--
+
